d5f22f93e51145482fe4f02294f34fd5efceeea9
Rameez Israr <rameez@govassist.com>
2024-09-05 21:12:56 +0500

    Create Message controller and a request class for validation

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\MessageRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class MessageController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function create(MessageRequest $request)
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+}
